97f508c3c5d435c1c934dc15e06418bcb03d1806
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-17 00:18:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,7 +72,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = {
+     adding_matz = programmer_hash[:yukihiro_matsumoto] = {
      :known_for => "Ruby",
      :languages => ["LISP", "C"]}
 
